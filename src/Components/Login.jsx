e7f021283dd5b1636883daec2312a69fd45c5013
AlekseySpirin <spirin.mezhmun@yandex.ru>
2023-05-30 20:39:10 +0300

    Настроил валидацию для log и reg, продолжаю рефакторить.

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,53 +1,58 @@
 import React, {useState} from 'react';
 import './styles/Login.css';
+import {useFormAndValidation} from "../utils/hooks/useFormAndValidation";
 
-// import {Link, useNavigate} from 'react-router-dom';
 const Login = ({handleLogin}) => {
-	const [formValue, setFormValue] = useState({
-		email: '',
-		password: '',
-	});
+	const {
+		values,
+		handleChange,
+		errors,
+		resetForm
+	} = useFormAndValidation({email: '', password: ''});
 	
 	const [errorMessage, setErrorMessage] = useState('');
 	
-	const handleChange = (e) => {
-		const {name, value} = e.target;
-		
-		setFormValue({
-			...formValue,
-			[name]: value
-		});
-	};
-	
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		const {email, password} = formValue;
-		handleLogin(email, password).catch(err => setErrorMessage(err));
+		
+		handleLogin({
+			email: values.email,
+			password: values.password
+		}, resetForm).catch(err => setErrorMessage(err));
 		
 	};
 	
 	return (
 		<div className={'login'}>
-			<p className={'login__welcome'}>Вход</p>
+			<h2 className={'login__welcome'}>Вход</h2>
 			<p className={'login__error'}>{errorMessage}</p>
-			<form onSubmit={handleSubmit} className={'login__form'}>
-				<input placeholder={'Email'}
+			<form onSubmit={handleSubmit}
+			      className={'login__form'}
+			      noValidate>
+				<input placeholder="Email"
 				       required
-				       id={'email'}
-				       name={'email'}
-				       type="text"
+				       id="email"
+				       name="email"
+				       type="email"
+				       minLength="2"
+				       maxLength="30"
 				       onChange={handleChange}
-				       value={formValue.email}
+				       value={values.email}
 				/>
-				
+				{errors.email && <span
+					className="form__item-error form__item-error_el_login">{errors.email}</span>}
 				<input placeholder={'Пароль'}
 				       required
-				       id={'password'}
-				       name={'password'}
-				       type={'password'}
+				       id="password"
+				       name="password"
+				       type="password"
+				       minLength="2"
+				       maxLength="30"
 				       onChange={handleChange}
-				       value={formValue.password}
+				       value={values.password}
 				/>
+				{errors.password && <span
+					className="form__item-error form__item-error_el_place">{errors.password}</span>}
 				<div className="login__button-container">
 					<button type="submit"
 					        className="login__link">Войти
